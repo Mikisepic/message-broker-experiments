73abe14eb1bbbdf4a7c0f4183b48a05d07750ff1
Mikas Pupeikis <mikas.pup@gmail.com>
2023-01-09 22:53:51 +0200

    Apache kafka

---
+++ apache-kafka/app/produce.py
@@ -0,0 +1,14 @@
+from kafka import KafkaProducer
+
+# Connect to the Kafka broker
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
+
+# Send the messages
+for i in range(10):
+    producer.send('my_topic', b'Message {}'.format(i))
+
+print("Sent 10 messages to the topic.")
+
+# Flush and close the producer
+producer.flush()
+producer.close()
